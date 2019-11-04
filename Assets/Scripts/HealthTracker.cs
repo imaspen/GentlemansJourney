a1a8c68d443196ceb790250c2877baecb33efd01
Danny Haynes <graafoxx@gmail.com>
2019-11-04 13:52:27 +0000

    Health Tracking Script

---
+++ Assets/Scripts/HealthTracker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTracker : MonoBehaviour
+{
+    [SerializeField]
+   private float _currentHealth;
+
+    public float CurrentHealth
+    {
+        get { return _currentHealth; }
+        set { _currentHealth = Mathf.Min(value, MaxHealth); }
+    }
+
+    [SerializeField]
+    private float _maxHealth;
+
+    public float MaxHealth
+    {
+        get { return _maxHealth; }
+        set { _maxHealth = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    void ReduceHealth(float reduction)
+    {
+        CurrentHealth = CurrentHealth - reduction;
+    }
+
+    void AddHealth(float hp)
+    {
+        CurrentHealth = CurrentHealth + hp;
+    }
+}
