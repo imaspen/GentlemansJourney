6ff0cd3da16f7ad714fe43a034c57ab4e9d26b87
Aspen Thompson <zebington@gmail.com>
2019-11-05 17:12:10 +0000

    Pathfinding!

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class EnemyController : MonoBehaviour
     public float rateOfFire = 2.0f;
     [Tooltip("Enemy will stay this distance from the player" +
         "     N/A for melee enemies")]
-    public float maxDist = 5.0f;
+    public float maxDist = 2.0f;
 
     [Header ("Ranged Or Melee?")]
     public bool isRanged;
@@ -22,22 +23,40 @@ public class EnemyController : MonoBehaviour
     // private variables
     private float _fireCooldown = 0.0f;
     private Transform _target;
+    private NavMeshAgent _agent;
+    private float _navmeshCooldown;
 
     private void Start()
     {
-        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        _target = player.transform;
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _navmeshCooldown += Time.deltaTime;
+        if (true)
+        {
+            if (Vector3.Distance(transform.position, _target.position) < maxDist)
+            {
+                _agent.isStopped = true;
+            }
+            else
+            {
+                _agent.isStopped = false;
+                _agent.SetDestination(_target.position);
+                _navmeshCooldown -= 1;
+            }
+        }
         // rotate to face player
         transform.LookAt(new Vector3(_target.position.x, transform.position.y, _target.position.z));
 
-        if (Vector3.Distance(transform.position, _target.position) >= maxDist)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, _target.position, moveSpeed * Time.deltaTime);
-        }
+        //if (Vector3.Distance(transform.position, _target.position) >= maxDist)
+        //{
+        //    transform.position = Vector3.MoveTowards(transform.position, _target.position, moveSpeed * Time.deltaTime);
+        //}
 
         if (isRanged) // handle ranged combat
         {
@@ -54,13 +73,13 @@ public class EnemyController : MonoBehaviour
             }
             _fireCooldown -= Time.deltaTime;
         }
-            else if (isMelee) // handle melee combat
-            {
-                // get close to the player
-                maxDist = 0.25f;
-                // stab
-                Melee();
-            }        
+        else if (isMelee) // handle melee combat
+        {
+            // get close to the player
+            maxDist = 0.25f;
+            // stab
+            Melee();
+        }        
     }
 
     void Shoot()
