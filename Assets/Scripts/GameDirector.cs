e7dc8fc3c2dcc0ed510a1a6ba0e902b9ff9f5e1c
Danny Haynes <graafoxx@gmail.com>
2019-11-06 16:53:08 +0000

    - added game director prefab - added death screen

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject deathScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player)
+        {
+            Instantiate(deathScreen);
+        }
+    }
+}
