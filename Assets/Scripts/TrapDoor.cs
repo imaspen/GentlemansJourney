5fd87c552e4c2bf8436320bad126024c3730b248
Aspen Thompson <zebington@gmail.com>
2019-11-07 18:44:44 +0000

    No Comments

--- Assets/Scripts/TrapDoor.cs
+++ Assets/Scripts/TrapDoor.cs
@@ -5,28 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class TrapDoor : MonoBehaviour
 {
+    private Collider _player;
+    private Collider _collider;
+    private float _enterTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = GameObject.FindGameObjectWithTag("Player")
+            .GetComponent<Collider>();
+        _collider = GetComponent<Collider>();
+        _enterTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
+        if (Input.GetAxis("Use") > 0.5 
+            && Time.time - _enterTime > 0.5
+            && _player.bounds.Intersects(_collider.bounds))
         {
-            if (Input.GetAxis("Use") > 0.5)
-            {
-                Debug.Log("Trapdoor used");
-                SceneManager.LoadScene(2);
-            }
+            SceneManager.LoadScene(2);
         }
-
     }
 }
