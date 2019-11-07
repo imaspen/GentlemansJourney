71da7e2efde3b5c76b140b2f44cd7b6a6255e852
Danny Haynes <graafoxx@gmail.com>
2019-11-07 23:45:08 +0000

    - added footsteps to player

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip[] footstepClips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private AudioClip GetRandomFootstep()
+    {
+        return footstepClips[UnityEngine.Random.Range(0, footstepClips.Length)];
+    }
+
+    public void FootStep()
+    {
+        audioSource.pitch = Random.Range(0.5f, 1.5f);
+        AudioClip clip = GetRandomFootstep();
+        audioSource.PlayOneShot(clip);
+    }
+}
